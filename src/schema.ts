59a8842166027c01a4ab48f3555f8630c085ebcb
Bashar Harfoush <bashar.harfoush@gmail.com>
2023-07-19 13:27:47 -0400

    basic graphql server

---
+++ src/schema.ts
@@ -0,0 +1,29 @@
+export const typeDefs = `#graphql
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+export const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
