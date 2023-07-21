67631f38233ebe98917bb99a0eca459464d78b47
Bashar Harfoush <bashar.harfoush@gmail.com>
2023-07-21 03:01:15 -0400

    add port in env

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -8,7 +8,7 @@ const NODE_ENV = process.env.NODE_ENV;
 
 const common = {
   host: process.env.POSTGRES_HOST,
-  port: 5432,
+  port: parseInt(process.env.POSTGRES_PORT ?? '5432'),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
